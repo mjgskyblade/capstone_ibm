5a9166258a4bee2baf021a73a1ae0260f5259b63
mjgskyblade <mgitembo@gmail.com>
2024-03-22 17:59:18 -0400

    added pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,16 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
-  );
-}
+    return (
+      <Router> 
+        <Routes>
+          <Route path="/register" element={<Register />} /> 
+          <Route path="/login" element={<LoginPanel />} />
+        </Routes>
+      </Router>
+    );
+  }
+  
 export default App;
