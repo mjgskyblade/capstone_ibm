778b0d4f6f90bd26faac98b0237e3fe13e5805cf
mjgskyblade <mgitembo@gmail.com>
2024-03-23 19:17:01 -0400

    API endpoints implemented

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,37 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships' });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+const state = req.params.state;
+  try {
+    const documents = await Dealerships.find({ state: state });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealerships by state' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+const id = req.params.id;
+  try {
+    const document = await Dealerships.findOne({ id: id });
+    res.json(document);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching dealer by ID' });
+  }
+
 });
 
 //Express route to insert review
