e81404282e95b66f60d436d20193a9b48708801a
mjgskyblade <mgitembo@gmail.com>
2024-03-23 20:36:17 -0400

    cars implemented

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'make', 'type', 'year')
+    list_filter = ('make', 'type', 'year')
+    search_fields = ('name', 'make__name')
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    search_fields = ('name',)
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,51 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    # Define the fields for the Car Make model
+
+    # - Name
+    name = models.CharField(max_length=100)
+
+    # - Description
+    description = models.TextField()
+
+    # - Any other fields you would like to include in car make model
+
+    # - __str__ method to print a car make object
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    # - Name
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    # - Type (CharField with a choices argument to provide limited choices
+    # such as Sedan, SUV, WAGON, etc.)
+    TYPE_CHOICES = (
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+    )
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+
+    # - Year (IntegerField) with min value 2015 and max value 2023
+    year = models.IntegerField(validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+
+    # - Any other fields you would like to include in car model
+
+    # - __str__ method to print a car make object
+    def __str__(self):
+        return f"{self.make.name} - {self.name}"
