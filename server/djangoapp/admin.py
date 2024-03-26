749cca32acbcf53aae828530122e1648168e6680
mjgskyblade <mgitembo@gmail.com>
2024-03-26 07:00:02 -0400

    yml corrections

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,17 +6,20 @@ class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'make', 'type', 'year')
     list_filter = ('make', 'type', 'year')
     search_fields = ('name', 'make__name')
 
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
     search_fields = ('name',)
     inlines = [CarModelInline]
+
 
 # Register models here
 admin.site.register(CarMake)
